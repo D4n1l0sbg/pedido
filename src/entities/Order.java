c1ef1d15a1def882fd84c745334ed4665457cc58
Danilo Souza <danilosbg50@gmail.com>
2022-12-21 21:21:51 -0300

    Comclusao da Aplicação

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,5 +1,6 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -7,6 +8,8 @@ import java.util.List;
 import entities.enums.OrderStatus;
 
 public class Order {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	
 	private Date moment;
 	//Composition
@@ -65,11 +68,30 @@ public class Order {
 	}
 	
 	public Double total() {
+		double sum = 0.0;
+		for (OrderItem it : orderItem) {
+			
+			sum+= it.subTotal();
+		}
 		
-		
-		
-		return null; 
-				
+		return sum; 		
 	}
 
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Order moment: ");
+		sb.append(sdf.format(moment) + "\n");
+		sb.append("Order status: ");
+		sb.append(status + "\n");
+		sb.append("Client: ");
+		sb.append(client + "\n");
+		sb.append("Order items:\n");
+		for (OrderItem item : orderItem) {
+			sb.append(item + "\n");
+		}
+		
+		sb.append("Total price: $");
+		sb.append(String.format("%.2f", total()));
+		return sb.toString();
+	}
 }
